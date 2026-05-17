86c63320e76a9556f2a757ab5323a16a3b1970d7
LeeYoosung <zooin25@gmail.com>
2026-05-17 12:26:01 +0900

    Refactor: Add documentation and use constants in GameManager

--- res/core/GameManager.cs
+++ res/core/GameManager.cs
@@ -5,14 +5,34 @@ using ProjectFR.Systems;
 
 namespace ProjectFR.Core;
 
+/// <summary>
+/// Global game manager singleton - manages game state and battle lifecycle
+/// </summary>
 public partial class GameManager : Node
 {
+    /// <summary>
+    /// Singleton instance of the GameManager
+    /// </summary>
     public static GameManager Instance { get; private set; } = null!;
 
+    /// <summary>
+    /// Current active battle, or null if no battle is in progress
+    /// </summary>
     public BattleManager? CurrentBattle { get; private set; }
+    
+    /// <summary>
+    /// Global clipboard system shared across battles
+    /// </summary>
     public ClipboardSystem Clipboard { get; } = new();
+    
+    /// <summary>
+    /// Global status effect system shared across battles
+    /// </summary>
     public StatusEffectSystem StatusEffects { get; } = new();
 
+    /// <summary>
+    /// Called when the node enters the scene tree
+    /// </summary>
     public override void _EnterTree()
     {
         if (Instance != null)
@@ -24,18 +44,42 @@ public partial class GameManager : Node
         SetProcessUnhandledInput(true);
     }
 
+    /// <summary>
+    /// Starts a new battle with default player and enemies
+    /// </summary>
     public void StartNewBattle()
     {
-        var player = new ActorState(maxHp: 30, maxAp: 5, attackPower: 4);
+        var player = new ActorState(
+            maxHp: BattleConstants.DefaultPlayerMaxHp,
+            maxAp: BattleConstants.DefaultPlayerMaxAp,
+            attackPower: BattleConstants.DefaultPlayerAttackPower
+        );
         CurrentBattle = new BattleManager(player);
 
-        CurrentBattle.AddEnemy(new ActorState(maxHp: 10, maxAp: 3, attackPower: 2));
-        CurrentBattle.AddEnemy(new ActorState(maxHp: 8, maxAp: 2, attackPower: 2));
-        CurrentBattle.AddEnemy(new ActorState(maxHp: 15, maxAp: 4, attackPower: 3));
+        CurrentBattle.AddEnemy(new ActorState(
+            maxHp: BattleConstants.DefaultEnemy1MaxHp,
+            maxAp: BattleConstants.DefaultEnemy1MaxAp,
+            attackPower: BattleConstants.DefaultEnemy1AttackPower
+        ));
+        
+        CurrentBattle.AddEnemy(new ActorState(
+            maxHp: BattleConstants.DefaultEnemy2MaxHp,
+            maxAp: BattleConstants.DefaultEnemy2MaxAp,
+            attackPower: BattleConstants.DefaultEnemy2AttackPower
+        ));
+        
+        CurrentBattle.AddEnemy(new ActorState(
+            maxHp: BattleConstants.DefaultEnemy3MaxHp,
+            maxAp: BattleConstants.DefaultEnemy3MaxAp,
+            attackPower: BattleConstants.DefaultEnemy3AttackPower
+        ));
 
         CurrentBattle.StartBattle();
     }
 
+    /// <summary>
+    /// Ends the current battle
+    /// </summary>
     public void EndCurrentBattle()
     {
         CurrentBattle = null;
